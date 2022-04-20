e28bde27e5a801b4dee6aedcca46c0329575dd39
tyleralc <91850177+tyleralc@users.noreply.github.com>
2022-04-19 21:51:34 -0700

    removed useless files

--- HW3/hw3.py
+++ HW3/hw3.py
@@ -3,7 +3,10 @@ from PIL import Image
 import pytesseract as tess
 tess.pytesseract.tesseract_cmd=r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 #img= Image.open(r'data\aljazeera\03-01-22.png')
-img= Image.open('95-Most-Important-Words-List-in-English.png')
+img= Image.open('fox_3_12_1.png')
+img2= Image.open('row-2-column-1.png')
 text=tess.image_to_string(img)
+text2=tess.image_to_string(img2)
 print(text)
-
+print('________________________')
+print(text2)
