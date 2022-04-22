a5a47968e233194d002873ecfcff1bbcc761a425
tyleralc <91850177+tyleralc@users.noreply.github.com>
2022-04-21 21:04:19 -0700

    update

--- HW3/hw3.py
+++ HW3/hw3.py
@@ -3,10 +3,10 @@ from PIL import Image
 import pytesseract as tess
 tess.pytesseract.tesseract_cmd=r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 #img= Image.open(r'data\aljazeera\03-01-22.png')
-img= Image.open('fox_3_12_1.png')
-img2= Image.open('row-2-column-1.png')
+img= Image.open('fox_3_12_2.png')
+#img2= Image.open('row-1-column-1.png')
 text=tess.image_to_string(img)
-text2=tess.image_to_string(img2)
+#text2=tess.image_to_string(img2)
 print(text)
 print('________________________')
-print(text2)
+#print(text2)
