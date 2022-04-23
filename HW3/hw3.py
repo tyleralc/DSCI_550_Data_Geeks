60b0fb5105c92e2ae63f7877ab9ece30e1f48e4e
tyleralc <tyleralc@usc.edu>
2022-04-23 14:28:47 -0700

    updated hw.py to iterate

--- HW3/hw3.py
+++ HW3/hw3.py
@@ -1,12 +1,29 @@
 from PIL import Image
-
+import os
 import pytesseract as tess
+
+
 tess.pytesseract.tesseract_cmd=r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+
+
+fox=r'data/fox'
+directory = os.fsencode(fox)
+    
+for file in os.listdir(directory):
+    filename = os.fsdecode(file)
+    try:
+        img=Image.open(filename)
+        text=tess.image_to_string(img)
+        print()
+    except:
+        print('too big')
+
+
 #img= Image.open(r'data\aljazeera\03-01-22.png')
-img= Image.open('fox_3_12_1.png')
-img2= Image.open('row-2-column-1.png')
-text=tess.image_to_string(img)
-text2=tess.image_to_string(img2)
-print(text)
-print('________________________')
-print(text2)
+# img= Image.open('fox_3_12_1.png')
+# img2= Image.open('row-2-column-1.png')
+# text=tess.image_to_string(img)
+# text2=tess.image_to_string(img2)
+# print(text)
+# print('________________________')
+# print(text2)
