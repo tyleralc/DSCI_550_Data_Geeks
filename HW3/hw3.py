b1869d3a632fbba7ce79a65f23127e08c9f0c7dd
tyleralc <91850177+tyleralc@users.noreply.github.com>
2022-04-23 20:22:18 -0700

    files are too large...

--- HW3/hw3.py
+++ HW3/hw3.py
@@ -3,16 +3,6 @@ import os
 import pytesseract as tess
 
 tess.pytesseract.tesseract_cmd=r'C:\Program Files\Tesseract-OCR\tesseract.exe'
-<<<<<<< HEAD
-#img= Image.open(r'data\aljazeera\03-01-22.png')
-img= Image.open('fox_3_12_2.png')
-#img2= Image.open('row-1-column-1.png')
-text=tess.image_to_string(img)
-#text2=tess.image_to_string(img2)
-print(text)
-print('________________________')
-#print(text2)
-=======
 
 
 # fox=r'data/fox'
@@ -28,7 +18,7 @@ print('________________________')
 #         print('too big')
 
 
-img= Image.open(r'data\fox\03-12-22-20.png')
+img= Image.open('03-12-22-1.png')
 #img= Image.open('95-Most-Important-Words-List-in-English.png')
 # img2= Image.open('row-2-column-1.png')
 text=tess.image_to_string(img)
@@ -36,4 +26,3 @@ text=tess.image_to_string(img)
 print(text)
 # print('________________________')
 # print(text2)
->>>>>>> 7e1a96687d148108de3033773638056d03279a02
