7e1a96687d148108de3033773638056d03279a02
tyleralc <tyleralc@usc.edu>
2022-04-23 17:23:45 -0700

    iterate through files

--- HW3/hw3.py
+++ HW3/hw3.py
@@ -2,28 +2,27 @@ from PIL import Image
 import os
 import pytesseract as tess
 
-
 tess.pytesseract.tesseract_cmd=r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
 
-fox=r'data/fox'
-directory = os.fsencode(fox)
+# fox=r'data/fox'
+# directory = os.fsencode(fox)
     
-for file in os.listdir(directory):
-    filename = os.fsdecode(file)
-    try:
-        img=Image.open(filename)
-        text=tess.image_to_string(img)
-        print()
-    except:
-        print('too big')
+# for file in os.listdir(directory):
+#     filename = os.fsdecode(file)
+#     try:
+#         img=Image.open(filename)
+#         text=tess.image_to_string(img)
+#         print(text)
+#     except:
+#         print('too big')
 
 
-#img= Image.open(r'data\aljazeera\03-01-22.png')
-# img= Image.open('fox_3_12_1.png')
+img= Image.open(r'data\fox\03-12-22-20.png')
+#img= Image.open('95-Most-Important-Words-List-in-English.png')
 # img2= Image.open('row-2-column-1.png')
-# text=tess.image_to_string(img)
+text=tess.image_to_string(img)
 # text2=tess.image_to_string(img2)
-# print(text)
+print(text)
 # print('________________________')
 # print(text2)
