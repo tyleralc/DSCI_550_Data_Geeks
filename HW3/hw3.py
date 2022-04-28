198b4c297253da3cb326bce78713396d4359f066
tyleralc <91850177+tyleralc@users.noreply.github.com>
2022-04-28 09:02:29 -0700

    l

--- HW3/hw3.py
+++ HW3/hw3.py
@@ -5,23 +5,25 @@ import pytesseract as tess
 tess.pytesseract.tesseract_cmd=r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
 
-# cnn_test= 'data//3-12-22cnn'
-# directory = os.fsencode(cnn_test)
+cnn_test= r'C:\Users\Tyler Alcorn\OneDrive - University of Southern California\Documents\GitHub\DSCI_550_Data_Geeks\HW3\data\cnn'
+directory = os.fsencode(cnn_test)
 
-# for file in os.listdir(directory):
-#     #filename = os.fsdecode(file)
-#     try:
-#         img=Image.open(file)
-#         text=tess.image_to_string(img)
-#         print(text)
-#     except:
-#         print('too big')
+for file in os.listdir(cnn_test):
+    print(file)
+    filename = os.fsdecode(file)
+    
+    try:
+        img=Image.open(filename)
+        text=tess.image_to_string(img)
+        print(text)
+    except:
+        print('too big')
 
-img= Image.open('03-12-22-1.png')
-# # #img= Image.open('95-Most-Important-Words-List-in-English.png')
-# # # img2= Image.open('row-2-column-1.png')
-text=tess.image_to_string(img)
-# # # text2=tess.image_to_string(img2)
-print(text)
+# img= Image.open('\\data\\cnn\\03-12-22-1.png')
+# # # # #img= Image.open('95-Most-Important-Words-List-in-English.png')
+# # # # # img2= Image.open('row-2-column-1.png')
+# text=tess.image_to_string(img)
+# # # # # text2=tess.image_to_string(img2)
+# print(text)
 # print('________________________')
 # print(text2)
